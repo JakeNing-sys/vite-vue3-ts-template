cd554f323d9840ad0afce076288ca3a1243bd544
wangyaning <8040020+jakening@user.noreply.gitee.com>
2024-04-12 16:29:13 +0800

    feat: 'add'

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
-import zhCN from 'ant-design-vue/es/locale/zh_CN'
+// import zhCN from 'ant-design-vue/es/locale/zh_CN'
 // import enGB from 'ant-design-vue/es/locale/en_GB'
 
 import dayjs from 'dayjs'
 import 'dayjs/locale/zh-cn'
 import 'dayjs/locale/en'
 
-const locale = ref(zhCN)
-const current = ref(6)
+// const locale = ref(zhCN)
+// const current = ref(6)
 
 dayjs.locale('zh-cn')
 </script>
